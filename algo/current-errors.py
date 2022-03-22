0a9d8c7766a0240134ee8a71232125f2b7e933e0
Thibault Collin <99095146+thibaultcoo@users.noreply.github.com>
2022-03-22 16:41:55 +0100

    Delete current-errors.py

--- algo/current-errors.py
+++
@@ -1,7 +0,0 @@
-# list of errors that I am having right now
-
-# module vanillaMainAlgo
-# line 80: geometricBrownianMotion object is not subscriptable
-
-# module multiJumpDiffusion
-# dont know how to generate random Poisson term
